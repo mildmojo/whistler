1a0435bb9d40eb615ec6650c37bd057c1fb50688
mildmojo <theaterfire@yahoo.com>
2015-09-14 17:07:04 -0400

    Fixes uglifyjs argument order, adds `-c -m`.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 webpack
-uglifyjs --screw-ie8 build.js > build_new.js
+uglifyjs build.js --screw-ie8 -c -m > build_new.js
 mv build_new.js build.js
 zip -r whistlers_motive index.html build.js css/
 du -b whistlers_motive.zip
